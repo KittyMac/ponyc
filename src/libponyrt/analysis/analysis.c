08a91f69dbaa0a8dc3b00010035ccb91bb6f38b5
Rocco Bowling <rocco@chimerasw.com>
2019-12-04 17:25:30 -0500

    analysis event logging is now offloaded to its own thread to have less negative affect on the main pony program

--- src/libponyrt/analysis/analysis.c
+++ src/libponyrt/analysis/analysis.c
@@ -6,6 +6,12 @@
 #include <stdio.h>
 #include <sys/time.h>
 
+#ifdef PLATFORM_IS_WINDOWS
+// TODO: what is needed for windows sleep?
+#else
+#include <unistd.h>
+#endif
+
 #include "../pony.h"
 #include "../actor/actor.h"
 #include "../sched/scheduler.h"
@@ -18,10 +24,34 @@
 
 #define UNUSED(x) (void)(x)
 
-// Save timed runtime events and information for later analysis
-static FILE * analyticsFile = NULL;
+
+
+typedef struct analysis_msg_t
+{
+	pony_msg_t msg;
+	
+	unsigned long timeOfEvent;
+	unsigned long fromUID;
+	unsigned long fromTag;
+	unsigned long eventID;
+	unsigned long fromNumMessages;
+	unsigned long fromBatch;
+	unsigned long fromPriority;
+	unsigned long fromHeapUsed;
+	
+	unsigned long toUID;
+	unsigned long toTag;
+	unsigned long toNumMessages;
+	unsigned long totalMemory;	
+	
+} analysis_msg_t;
+
+
 static uint64_t startMilliseconds = 0;
 static bool analysisEnabled = false;
+static pony_thread_id_t analysisThreadID;
+static bool analysisThreadRunning = false;
+static messageq_t analysisMessageQueue;
 
 void ponyint_analysis_setanalysis(bool state) {
 	analysisEnabled = state;
@@ -32,39 +62,110 @@ bool ponyint_analysis_getanalysis() {
 }
 
 
-uint64_t timeInMilliseconds() {
+uint64_t ponyint_analysis_timeInMilliseconds() {
 	struct timeval timeOfDay;
 	gettimeofday(&timeOfDay, NULL);
 	return timeOfDay.tv_sec * 1000 + (timeOfDay.tv_usec / 1000);
 }
 
+DECLARE_THREAD_FN(analysisEventStorageThread)
+{
+	UNUSED(arg);
+	
+	// 1. open a file in temp
+	// 2. poll the mqm we were given for events
+	// 3. write the events to the file
+	// 4. end if analysisThreadRunning is false
+	
+	// Save timed runtime events and information for later analysis
+	FILE * analyticsFile = NULL;
+	
+#ifdef PLATFORM_IS_IOS
+	char path[1024];
+	snprintf(path, 1024, "%s/pony.ponyrt_analytics", ponyapp_tempDirectory());
+	analyticsFile = fopen(path, "w");
+#else
+	analyticsFile = fopen("/tmp/pony.ponyrt_analytics", "w");
+#endif
+	
+	if (analyticsFile == NULL) {
+		analysisEnabled = false;
+		analysisThreadRunning = false;
+		return NULL;
+	}
+	
+	fprintf(analyticsFile, "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n", 
+		"TIME_OF_EVENT",
+		"ACTOR_A_UUID",
+		"ACTOR_A_TAG", 
+		"EVENT_NUMBER", 
+		"ACTOR_A_NUMBER_OF_MESSAGES", 
+		"ACTOR_A_BATCH_SIZE", 
+		"ACTOR_A_PRIORITY", 
+		"ACTOR_A_HEAP_SIZE", 
+		"ACTOR_B_UUID", 
+		"ACTOR_B_TAG", 
+		"ACTOR_B_NUMBER_OF_MESSAGES",
+		"TOTAL_MEMORY"
+		);
+	
+	analysis_msg_t * msg;
+	while (analysisThreadRunning) {
+		while((msg = (analysis_msg_t*)ponyint_thread_messageq_pop(&analysisMessageQueue
+			#ifdef USE_DYNAMIC_TRACE
+			, SPECIAL_THREADID_ANALYSIS
+			#endif
+		)) != NULL)
+		{
+			
+			fprintf(analyticsFile, "%lu,%lu,%lu,%lu,%lu,%lu,%lu,%lu,%lu,%lu,%lu,%lu\n", 
+					msg-> timeOfEvent,
+					msg->fromUID,
+					msg->fromTag,
+					msg->eventID,
+					msg->fromNumMessages,
+					msg->fromBatch,
+					msg->fromPriority,
+					msg->fromHeapUsed,
+					msg->toUID,
+					msg->toTag,
+					msg->toNumMessages,
+					msg->totalMemory
+				);
+		}
+		
+#ifdef PLATFORM_IS_WINDOWS
+  Sleep(5000);
+#else
+  usleep(5000);
+#endif
+	}
+	
+	fclose(analyticsFile);
+	
+	return NULL;
+}
+
 void confirmRuntimeAnalyticHasStarted() {
+	
 	if (analysisEnabled == false) {
 		return;
 	}
 	
-	// simple CSV file with TIME,ACTOR_TAG,ANALYTIC_EVENT,NUMBER_OF_MESSAGES,BATCH_SIZE,PRIORITY,TO_ACTOR_TAG,TO_ACTOR_NUMBER_OF_MESSAGES
-	if (analyticsFile == NULL) {
-		analyticsFile = fopen("/tmp/pony.ponyrt_analytics", "w");
+	// We use a thread to not slow down the rest of the pony program with storing the events to disk
+	if (analysisThreadRunning == false) {
+		analysisThreadRunning = true;
 		
-		fprintf(analyticsFile, "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n", 
-			"TIME_OF_EVENT",
-			"ACTOR_A_UUID",
-			"ACTOR_A_TAG", 
-			"EVENT_NUMBER", 
-			"ACTOR_A_NUMBER_OF_MESSAGES", 
-			"ACTOR_A_BATCH_SIZE", 
-			"ACTOR_A_PRIORITY", 
-			"ACTOR_A_HEAP_SIZE", 
-			"ACTOR_B_UUID", 
-			"ACTOR_B_TAG", 
-			"ACTOR_B_NUMBER_OF_MESSAGES",
-			"TOTAL_MEMORY"
-			);
+		startMilliseconds = ponyint_analysis_timeInMilliseconds();
 		
-		startMilliseconds = timeInMilliseconds();		
+		ponyint_messageq_init(&analysisMessageQueue);
+		
+	    if(!ponyint_thread_create(&analysisThreadID, analysisEventStorageThread, -1, NULL)) {
+			analysisThreadRunning = false;
+	    }
 	}
 }
+
 
 void saveRuntimeAnalyticForActorMessage(pony_actor_t * from, pony_actor_t * to, int event) {
 	if (analysisEnabled == false) {
@@ -73,21 +174,27 @@ void saveRuntimeAnalyticForActorMessage(pony_actor_t * from, pony_actor_t * to, 
 	
 	confirmRuntimeAnalyticHasStarted();
 	
-	if (analyticsFile != NULL && from != NULL && to != NULL && from->tag != 0 && to->tag != 0) {
-		fprintf(analyticsFile, "%lu,%lu,%lu,%lu,%lu,%lu,%lu,%lu,%lu,%lu,%lu,%lu\n", 
-			(unsigned long)(timeInMilliseconds() - startMilliseconds),
-			(unsigned long)from->uid,
-			(unsigned long)from->tag, 
-			(unsigned long)event, 
-			(unsigned long)from->q.num_messages,
-			(unsigned long)100, //from->batch,
-			(unsigned long)0, //from->priority,
-			(unsigned long)from->heap.used,
-			(unsigned long)to->uid,
-			(unsigned long)to->tag,
-			(unsigned long)to->q.num_messages,
-			(unsigned long)ponyint_total_memory()
-			);
+	if (analysisThreadRunning && from != NULL && to != NULL && from->tag != 0 && to->tag != 0) {
+		
+	    analysis_msg_t * msg = (analysis_msg_t*) pony_alloc_msg(POOL_INDEX(sizeof(analysis_msg_t)), 0);
+		msg->timeOfEvent = (unsigned long)(ponyint_analysis_timeInMilliseconds() - startMilliseconds);
+		msg->fromUID = from->uid;
+		msg->fromTag = from->tag;
+		msg->eventID = event;
+		msg->fromNumMessages = (unsigned long)from->q.num_messages;
+		msg->fromBatch = 0;
+		msg->fromPriority = 0;
+		msg->fromHeapUsed = from->heap.used;
+		msg->toUID = to->uid;
+		msg->toTag = to->tag;
+		msg->toNumMessages = (unsigned long)to->q.num_messages;
+		msg->totalMemory = ponyint_total_memory();
+			
+		ponyint_thread_messageq_push(&analysisMessageQueue, (pony_msg_t*)msg, (pony_msg_t*)msg
+		#ifdef USE_DYNAMIC_TRACE
+					, SPECIAL_THREADID_ANALYSIS, SPECIAL_THREADID_ANALYSIS
+		#endif
+	      );
 	}
 }
 
@@ -98,24 +205,37 @@ void saveRuntimeAnalyticForActor(pony_actor_t * actor, int event) {
 	
 	confirmRuntimeAnalyticHasStarted();
 	
-	if (analyticsFile != NULL && actor != NULL && actor->tag != 0) {
-		fprintf(analyticsFile, "%lu,%lu,%lu,%lu,%lu,%lu,%lu,%lu,0,0,0,%lu\n", 
-			// nanoseconds -> milliseconds
-			(unsigned long)(timeInMilliseconds() - startMilliseconds),
-			(unsigned long)actor->uid,
-			(unsigned long)actor->tag, 
-			(unsigned long)event, 
-			(unsigned long)actor->q.num_messages,
-			(unsigned long)100, //actor->batch,
-			(unsigned long)0, //actor->priority,
-			(unsigned long)actor->heap.used,
-			(unsigned long)ponyint_total_memory()
-			);
+	if (analysisThreadRunning && actor != NULL && actor->tag != 0) {
+		
+	    analysis_msg_t * msg = (analysis_msg_t*) pony_alloc_msg(POOL_INDEX(sizeof(analysis_msg_t)), 0);
+		msg->timeOfEvent = (unsigned long)(ponyint_analysis_timeInMilliseconds() - startMilliseconds);
+		msg->fromUID = actor->uid;
+		msg->fromTag = actor->tag;
+		msg->eventID = event;
+		msg->fromNumMessages = (unsigned long)actor->q.num_messages;
+		msg->fromBatch = 0;
+		msg->fromPriority = 0;
+		msg->fromHeapUsed = actor->heap.used;
+		msg->toUID = 0;
+		msg->toTag = 0;
+		msg->toNumMessages = 0;
+		msg->totalMemory = ponyint_total_memory();
+			
+		ponyint_thread_messageq_push(&analysisMessageQueue, (pony_msg_t*)msg, (pony_msg_t*)msg
+		#ifdef USE_DYNAMIC_TRACE
+					, SPECIAL_THREADID_ANALYSIS, SPECIAL_THREADID_ANALYSIS
+		#endif
+	      );
+
 	}
 }
 
 void endRuntimeAnalyticForActor() {
-	if (analyticsFile != NULL) {
-		fclose(analyticsFile);
+	
+	if (analysisThreadRunning) {
+		analysisThreadRunning = false;
+		ponyint_thread_join(analysisThreadID);
+		
+		ponyint_messageq_destroy(&analysisMessageQueue);
 	}
 }

f9c5c17a076014951d37e9f6bb4fc5d860b1c39c
Rocco Bowling <rocco@chimerasw.com>
2020-05-07 00:38:47 -0400

    tweaks to scheduler sleep timings

--- src/libponyrt/sched/scheduler.c
+++ src/libponyrt/sched/scheduler.c
@@ -778,8 +778,8 @@ static pony_actor_t* steal(scheduler_t* sched, bool local_ponyint_actor_getnoblo
   int scaling_sleep_min = 50;      // The minimum value we start actually sleeping at
   int scaling_sleep_max = 5000;     // The maximimum amount of time we are allowed to sleep at any single call
 #else
-  int scaling_sleep_delta = 10;
-  int scaling_sleep_min = 100;      // The minimum value we start actually sleeping at
+  int scaling_sleep_delta = 100;
+  int scaling_sleep_min = 50;      // The minimum value we start actually sleeping at
   int scaling_sleep_max = 500;     // The maximimum amount of time we are allowed to sleep at any single call
 #endif
     
